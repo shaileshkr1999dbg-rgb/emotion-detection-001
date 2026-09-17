a17e4a37d72855e0c10916eaceb8c4b85f36dff7
shaileshkr1999dbg-rgb <shaileshkr1999dbg@gmail.com>
2026-09-18 01:57:47 +0530

    Add files via upload

---
+++ emotional_app.py
@@ -0,0 +1,46 @@
+import streamlit as st
+import joblib
+
+# Load model
+model = joblib.load(
+    r"C:\Users\shail\Downloads\emotion_model.pkl"
+)
+
+# Load TF-IDF vectorizer
+vectorizer = joblib.load(
+    r"C:\Users\shail\Downloads\tfidf_vectorizer.pkl"
+)
+
+# Load emotion mapping
+emotion_numbers = joblib.load(
+    r"C:\Users\shail\Downloads\emotion_mapping.pkl"
+)
+
+# Reverse the mapping
+reverse_mapping = {v: k for k, v in emotion_numbers.items()}
+
+
+# Title
+st.title("😊 Emotion Detection using NLP")
+
+# Input
+text = st.text_area("Enter a sentence:")
+
+# Button
+if st.button("Predict Emotion"):
+
+    if text.strip() == "":
+        st.warning("Please enter some text.")
+
+    else:
+        # Convert input text into TF-IDF
+        text_vector = vectorizer.transform([text])
+
+        # Predict emotion number
+        prediction = model.predict(text_vector)[0]
+
+        # Convert number → emotion word
+        emotion = reverse_mapping[prediction]
+
+        # Show result
+        st.success(f"Predicted Emotion: {emotion}")
